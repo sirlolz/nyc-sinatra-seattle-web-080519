0fc3c40a806bca4216e408bc5f8023d2ff0e2c69
github username <github email address>
2019-08-29 15:36:52 -0700

    server side no errors figures passes all test no typos

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,24 @@
 class FiguresController < ApplicationController
+  Title.create(:name => "title1")
+  Title.create(:name => "title2")
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figure/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/new' do
+    # @titles = Title.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    erb :'figures/index'
+  end
 end
