c93809b26e78e14bd5a4730db7743c12010ab907
github username <github email address>
2019-08-29 13:32:59 -0700

    tables created

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,5 @@
 class Title < ActiveRecord::Base
-  # add relationships here
+  # add relationships 
+  has_many :firgure_titles
+  has_many :figures, through: :figure_titles
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change 
+    create_table :landmarks do |t|
+      t.text :name
+      t.integer :figure_id
+      t.text :year_completed
+    end
+  end
 end
